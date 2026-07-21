201377f7a2e75a48cb5a52807f433ddda10e73e0
igoradam23 <igor232323@icloud.com>
2026-07-21 14:02:22 +0200

    Fix Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
   reactStrictMode: true,
   poweredByHeader: false,
   allowedDevOrigins: ["127.0.0.1"],
